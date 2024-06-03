c64cc83d7c115424c18f4132d95905496cd849e8
garudapaksi-yusuf <158990958+garudapaksi-yusuf@users.noreply.github.com>
2024-06-03 11:55:24 +0700

    reorder & add limit

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -26,10 +26,10 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['garudapaksi-yusuf/BSDApp',
-        'garudapaksi-yusuf/Supermarket_Customers',
+        projects: ['garudapaksi-yusuf/TSA_Rice_Price',
         'garudapaksi-yusuf/Syarah_Price_Regression',
-        'garudapaksi-yusuf/TSA_Rice_Price'
+        'garudapaksi-yusuf/Supermarket_Customers',
+        'garudapaksi-yusuf/BSDApp'
         ], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
@@ -160,7 +160,7 @@ const CONFIG = {
   blog: {
     source: 'medium', // medium | dev
     username: 'garudapaksi.yusuf', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    limit: 3, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
